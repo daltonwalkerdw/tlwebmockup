672c8098aecfa18a13f1c820a3b3872fe9d503bd
Jason Ebanks <jasonebanks@Jasons-MacBook-Pro-3.local>
2021-12-26 12:08:49 -0500

    font variable update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
-import './App.css';
+import './Styles/FrontPage.scss';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
+      <div className="hero">
+        <h1>ThreatLocker</h1>
+        <h2>Add Zero Trust Policy-Driven Security to Your Endpoints</h2>
+      </div>
     </div>
   );
 }
