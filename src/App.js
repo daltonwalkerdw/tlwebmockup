d869655b49bdf21fa1b88c4f3d94c071a3487356
richardgreckijr <richardgreckijr@gmail.com>
2021-12-26 13:13:33 -0500

    AnnBar styling

--- src/App.js
+++ src/App.js
@@ -6,18 +6,12 @@ import {
   Route,
   Link
 } from "react-router-dom";
-<<<<<<< HEAD
-=======
 import AnnBar from "./Components/AnnBar"
->>>>>>> 1c672a8 (Announcment styling)
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-=======
       <AnnBar />
->>>>>>> 1c672a8 (Announcment styling)
       <div className="hero">
         <h1>ThreatLocker</h1>
         <h2>Add Zero Trust Policy-Driven Security to Your Endpoints</h2>
